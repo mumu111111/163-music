4ae8933557f92fe2ac2c334119b12fa58658e892
mumu111111 <2646709035@qq.com>
2018-03-14 19:41:42 +0800

    add qiniu

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ var server = http.createServer(function(request, response){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/json;charset=utf-8')
     response.setHeader('Access-Control-Allow-Origin', '*')
-    response.removeHeader('Date')
+    // response.removeHeader('Date')
 
     var config = fs.readFileSync('./qiniu-key.json')
     config = JSON.parse(config)
 
-    let {accessKey, secretKey} = config;
-    var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+    let {AccessKey, SecretKey} = config;
+    var mac = new qiniu.auth.digest.Mac(AccessKey, SecretKey);
     var options = {
       scope: '163-music',
     };
